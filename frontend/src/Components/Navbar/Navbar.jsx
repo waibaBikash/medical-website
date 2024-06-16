2d61ef1eff7b86602181cce5b74411e2f8005cc2
waibaBikash <kavrelibikash@gmail.com>
2024-06-16 20:56:51 +1000

    complete navbar

--- frontend/src/Components/Navbar/Navbar.jsx
+++ frontend/src/Components/Navbar/Navbar.jsx
@@ -3,8 +3,19 @@ import './Navbar.css';
 
 const Navbar = () => {
   return (
+    
     <div className='navbar'>
-
+       <div className="nav-logo">
+         <p className='name'>REAL HEALTH</p>
+         <p>Medical Centre</p>
+       </div>
+        <ul className='nav-menu'>
+           <li className='color-red'>HOME</li>
+           <li>SERVICES</li>
+           <li>OUR DOCTORS</li>
+           <li>ABOUT US</li>
+           <li>CONTACT US</li>
+        </ul>
     </div>
   )
 }
